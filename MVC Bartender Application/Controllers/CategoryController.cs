3f398e1045fa15036ac8510c114c0773016c2e29
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-08-26 07:51:52 -0400

    Added a local ApllicationDbContex to my CategoryController. It can now connect to Database.

--- MVC Bartender Application/Controllers/CategoryController.cs
+++ MVC Bartender Application/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Bartender_Application.Data;
 
 namespace MVC_Bartender_Application.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContex _db;
+
+        public CategoryController(ApplicationDbContex db)
+        {
+                _db = db;
+        }
+
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
+
             return View();
         }
     }
