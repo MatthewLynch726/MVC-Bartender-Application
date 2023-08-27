b94f83173ab7526f4cefba6200f63abf9e2d5e63
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-08-27 12:54:40 -0400

    Added Edit methods in Controller as well as create the Edit View for Category.

--- MVC Bartender Application/Controllers/CategoryController.cs
+++ MVC Bartender Application/Controllers/CategoryController.cs
@@ -45,6 +45,44 @@ namespace MVC_Bartender_Application.Controllers
             
             return View(obj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if(CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
 
--- MVC Bartender Application/Models/Category.cs
+++ MVC Bartender Application/Models/Category.cs
@@ -13,6 +13,7 @@ namespace MVC_Bartender_Application.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
