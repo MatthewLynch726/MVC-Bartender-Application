ae4bc0569cde7e90fc182a576b628b6f92d6ed40
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-08-27 13:40:19 -0400

    Added Delete Functionality as well as learned about ActionName. It allows you to rename a method in the Controller.

--- MVC Bartender Application/Controllers/CategoryController.cs
+++ MVC Bartender Application/Controllers/CategoryController.cs
@@ -76,12 +76,45 @@ namespace MVC_Bartender_Application.Controllers
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
             return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
         }
     }
 }
