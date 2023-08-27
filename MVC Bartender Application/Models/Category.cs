efc8eb42ed1e6030c0320375613354e911a8df6e
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-08-27 12:43:47 -0400

    Added Client Side Validation as well as a new Data Annotation.

--- MVC Bartender Application/Models/Category.cs
+++ MVC Bartender Application/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC_Bartender_Application.Models
 {
@@ -11,6 +12,7 @@ namespace MVC_Bartender_Application.Models
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
